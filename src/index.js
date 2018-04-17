93fe8b26a15e54d4d3a54a1c41ca97da9d2158be
jmangz <echou1@binghamton.edu>
2018-04-16 20:26:15 -0400

    new index

--- src/index.js
+++ src/index.js
@@ -29,14 +29,6 @@ window.onload = function () {
 
     tool.onKeyDown = function (event) {
         if (gameController) {
-<<<<<<< HEAD
-            if (event.key === 'up') {
-                gameController.initiateAction(actions.startMoving())
-            } else if (event.key === 'down') {
-                gameController.initiateAction(actions.stopMoving())
-            } else if (event.key === 'left') {
-            } else if (event.key === 'right') {
-=======
             switch (event.key) {
                 case 'left':
                     gameController.initiateAction(actions.startTurning('left'));
@@ -63,7 +55,6 @@ window.onload = function () {
                 case 'up':
                     gameController.initiateAction(actions.stopMoving());
                     break;
->>>>>>> 5eab39379b01c1bd75f52efeebad431a9bbee3cc
             }
         }
     };
@@ -74,33 +65,4 @@ window.onload = function () {
             redraw(currentState, project.activeLayer);
         }
     }
-<<<<<<< HEAD
-
-}
-
-
-function createShip({ x, y }, rotation) {
-    var radius = 20;
-    var angle = rotation - 30;
-
-    var circle = new Path.Circle(new Point(x, y), radius);
-    var triangle = new Path.RegularPolygon(new Point(x, y), 3, radius);
-    var triangle2 = new Path.RegularPolygon(new Point(x, y), 3, radius / 2);
-
-    triangle.insert(1, new Point(x, y));
-    triangle2.insert(1, new Point(x, y));
-
-    var ship = new CompoundPath({
-        children: [
-            triangle, triangle2, circle
-        ],
-        // selected: true
-    })
-    ship.strokeColor = 'cyan';
-    ship.strokeWidth = 2;
-    ship.rotate(angle);
-    return ship
-}
-=======
 };
->>>>>>> 5eab39379b01c1bd75f52efeebad431a9bbee3cc
